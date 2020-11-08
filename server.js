289f087fd5d62f296a09ee6a1176f7309435b30f
Dok <ddumansky@gmail.com>
2020-11-08 12:19:35 +0200

    CI config

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path      = require('path');
 const app       = express();
 const port      = 80;
 
+app.get('/', (req, res) => {
+	res.send('Videoflow WebRTC Samples');
+});
+
 app.get('/streamer', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/streamer.html'));
 });
